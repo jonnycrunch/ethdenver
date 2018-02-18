9107c704e0f02864aa4eded2674ec4dd7b10c3a9
Nick Neuman <nick.m.neuman@gmail.com>
2018-02-17 19:39:21 -0700

    Completed contract

--- keysplit-dapp/solidity/contracts/shardstore.sol
+++ keysplit-dapp/solidity/contracts/shardstore.sol
@@ -1,11 +1,21 @@
 pragma solidity ^0.4.18;
 
 contract ShardStore {
-    struct ShardHash {
-        uint shardId;
+
+    event StorageConfirmed(
         address trustedContact;
+        uint shardId;
+        );
+
+    function confirmStorage(uint shardId) public {
+        StorageConfirmed(msg.sender, shardId);
     }
 
-    
-
+    /* FUTURE WORK FOR EVALUATION
+    - Do we want to create a mapping of addresses that are allowed
+    to call confirmStorage? Right now a small attack vector exists
+    where a malicious actor could confirm storage of a particular
+    shardId even though the true user is not confirming that and may
+    no longer have the ID.
+    */
 }
